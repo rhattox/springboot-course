90708ca442c73702a655031ee9c6c81f62c1966c
Bruno Souto <65253099+bcovies@users.noreply.github.com>
2023-11-07 22:33:12 -0300

    Add Set Injection

--- 02-spring-boot-spring-core/02-component-scanning/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrincketCoach.java
+++ 02-spring-boot-spring-core/02-component-scanning/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrincketCoach.java
@@ -1,9 +1,9 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
 @Component
-public class CrincketCoach implements Coach{
+public class CrincketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fas bowling for 15 minutes";
--- 02-spring-boot-spring-core/03-setter-injection/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/03-setter-injection/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,5 +1,6 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,9 @@ public class DemoController {
 
     private Coach myCoach;
 
-    // defines a constructor for dependency injectione
+    // defines a constructor for dependency injection
     @Autowired
-    public DemoController (Coach theCoach){
+    public void setCoach(Coach theCoach){
         myCoach = theCoach;
     }
 
