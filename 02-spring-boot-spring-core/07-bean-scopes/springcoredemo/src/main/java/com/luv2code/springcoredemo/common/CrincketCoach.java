b8f9ff5bcc41d907d5b81861910dc0155f10b041
Bruno Souto <65253099+bcovies@users.noreply.github.com>
2023-11-13 22:57:05 -0300

    add beans scopes

--- 02-spring-boot-spring-core/07-bean-scopes/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrincketCoach.java
+++ 02-spring-boot-spring-core/07-bean-scopes/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrincketCoach.java
@@ -1,9 +1,11 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CrincketCoach implements Coach {
     public CrincketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
--- 02-spring-boot-spring-core/07-bean-scopes/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/07-bean-scopes/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
     public DemoController(){
         System.out.println("In constructor: " + getClass().getSimpleName());
@@ -17,12 +18,20 @@ public class DemoController {
 
     // defines a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("crincketCoach") Coach theCoach){
+    public DemoController(
+            @Qualifier("crincketCoach") Coach theCoach,
+            @Qualifier("crincketCoach") Coach theAnotherCoach){
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
